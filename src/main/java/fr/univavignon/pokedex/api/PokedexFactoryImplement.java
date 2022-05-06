d4241f22086568021f1ea17875b1dec0c58bf832
SYSAVANE73 <ahmadou_dia.sy_savane@alumni.univ-avignon.fr>
2022-05-06 12:01:49 +0200

    ajout de plugin checkstyle version 3

--- src/main/java/fr/univavignon/pokedex/api/PokedexFactoryImplement.java
+++ src/main/java/fr/univavignon/pokedex/api/PokedexFactoryImplement.java
@@ -5,11 +5,7 @@ public class PokedexFactoryImplement implements IPokedexFactory{
 	IPokemonMetadataProvider metadataProvider;
 	IPokemonFactory pokemonFactory;
 	
-	/*
-	public PokedexFactoryImplement() {
-		metadataProvider = new MetadataP
-	}
-	*/
+	
 	@Override
 	public IPokedex createPokedex(IPokemonMetadataProvider metadataProvider, IPokemonFactory pokemonFactory) {
 		// TODO Auto-generated method stub
